898778bff5c17f795994dac842c8bec7f5598b74
Neo Masilo <neolawrencemasilo@gmail.com>
2023-08-10 11:18:34 +0000

    checking for lowercase characters

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    while c:
-        if ord(c) in range(ord('a'), ord('z') + 1):
-            return True
-        else:
-            return False
+    if ord(c) in range(ord('a'), ord('z') + 1):
+        return True
+    else:
+        return False
