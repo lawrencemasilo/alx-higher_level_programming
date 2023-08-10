2dcba7331ef82f763fdedcaedc727d9917bc04ca
Neo Masilo <neolawrencemasilo@gmail.com>
2023-08-10 12:45:15 +0000

    printing uppercase string

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_case = ""
+    for char in str:
+        if ord(char) in range(ord('a'), ord('z')):
+            upper = chr(ord(char) - 32)
+            upper_case = upper_case + upper
+        else:
+            upper_case = upper_case + char
+    print("{}".format(upper_case), end="\n")
