754c6833a5f2ee633aaf223778be4164d6c64650
Neo Masilo <neolawrencemasilo@gmail.com>
2023-08-13 15:40:51 +0000

    fixing errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx < 0) | (idx > len(my_list)):
+    if (idx < 0):
         return None
-    else:
+    elif idx < len(my_list):
         return my_list[idx]
+    else:
+        return None
