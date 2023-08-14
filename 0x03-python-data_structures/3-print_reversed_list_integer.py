0a45f1148764c830e684fecd433746bb899d0052
Neo Masilo <neolawrencemasilo@gmail.com>
2023-08-14 12:45:53 +0000

    added integer format specifer to print()

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for index in range(len(my_list)):
-        print("{}".format(my_list[index]))
+        print("{:d}".format(my_list[index]))
