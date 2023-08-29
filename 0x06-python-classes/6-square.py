56e3340deee10b72321bfc13071293cff36ee47c
Neo Masilo <neolawrencemasilo@gmail.com>
2023-08-29 18:26:34 +0000

    added a position attribute

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""This module contains a class that defines a square"""
+
+
+class Square:
+    """ __inti__: to initialize size
+        self.__size: to make size private
+        try: is to check if size is an integer
+        @property: to initialize the getter function
+        @size.setter: to initialize the setter function
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def position(self):
+        return self.position
+
+    @position.setter
+    def position(self, value):
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def my_print(self):
+        if self.__size == 0:
+            print(end="\n")
+        else:
+            x = 0
+            for i in range(self.__size):
+                print("_", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print(end="\n")
