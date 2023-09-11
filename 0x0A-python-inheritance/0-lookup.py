78044589b372b865c4927cc886926ce8f99ae092
neo masilo <neolawrencemasilo@gmail.com>
2023-09-11 01:58:59 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+    This module contains a function that returns
+    attributes and methods of an object
+"""
+
 
 def lookup(obj):
+    """
+    parameter a: an object
+    """
     return list(dir(obj))
