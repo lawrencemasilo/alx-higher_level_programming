82bdb20f7f76b578a0d792c5e3ca0ab60a3ead1a
neo masilo <neolawrencemasilo@gmail.com>
2023-09-12 03:33:48 -0700

    a function that reads file and prints contents to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains a function that opens a file"""
+
+
+def read_file(filename=""):
+    """
+    Reads a file
+    Args:
+        filename: The name of the file to open/read
+    """
+    with open(filename, encoding="utf-8") as fo:
+        print(fo.read())
