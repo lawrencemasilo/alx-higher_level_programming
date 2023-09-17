40c637afd7e8f40f31a4c04802db645c2e16b9d6
Neo Masilo <neolawrencemasilo@gmail.com>
2023-09-17 11:44:32 +0000

    overrided the __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,15 @@ class Rectangle(Base):
 
     def area(self):
         return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print(end="\n")
+
+    def __str__(self):
+        return (
+                f"[Rectangle] ({self.id}) {self.__x}/"
+                f"{self.__y} - {self.__width}/{self.__height}"
+            )
