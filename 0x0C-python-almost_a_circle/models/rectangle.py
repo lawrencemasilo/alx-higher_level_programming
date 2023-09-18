d76133139f1665d5eb2978f1a27fffd11e0b31ed
Neo Masilo <neolawrencemasilo@gmail.com>
2023-09-18 09:21:48 +0000

    updated the update method, to be able to handle kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,33 +116,65 @@ class Rectangle(Base):
                 f"{self.__y} - {self.__width}/{self.__height}"
             )
 
-    def update(self, *args):
-        super().__init__(args[0])
+    def update(self, *args, **kwargs):
+        if args is not None or Kwargs is not None:
+            if len(args) > 0:
+                super().__init__(args[0])
+            if "id" in kwargs:
+                super().__init__(kwargs["id"])
 
-        if len(args) > 1:
-            if not isinstance(args[1], int):
-                raise TypeError("width must be an integer")
-            if args[1] <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = args[1]
+            if len(args) > 1:
+                if not isinstance(args[1], int):
+                    raise TypeError("width must be an integer")
+                if args[1] <= 0:
+                    raise ValueError("width must be > 0")
+                self.__width = args[1]
 
-        if len(args) > 2:
-            if not isinstance(args[2], int):
-                raise TypeError("height must be an integer")
-            if args[2] <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = args[2]
+            if "width" in kwargs:
+                if not isinstance(kwargs["width"], int):
+                    raise TypeError("width must be an integer")
+                if kwargs["width"] <= 0:
+                    raise ValueError("width must be > 0")
+                self.__width = kwargs["width"]
 
-        if len(args) > 3:
-            if not isinstance(args[3], int):
-                raise TypeError("x must be an integer")
-            if args[3] < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = args[3]
+            if len(args) > 2:
+                if not isinstance(args[2], int):
+                    raise TypeError("height must be an integer")
+                if args[2] <= 0:
+                    raise ValueError("height must be > 0")
+                self.__height = args[2]
 
-        if len(args) > 4:
-            if not isinstance(args[4], int):
-                raise TypeError("y must be an integer")
-            if args[4] < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = args[4]
+            if "height" in kwargs:
+                if not isinstance(kwargs["height"], int):
+                    raise TypeError("height must be an integer")
+                if kwargs["height"] <= 0:
+                    raise ValueError("height must be > 0")
+                self.__height = kwargs["height"]
+
+            if len(args) > 3:
+                if not isinstance(args[3], int):
+                    raise TypeError("x must be an integer")
+                if args[3] < 0:
+                    raise ValueError("x must be >= 0")
+                self.__x = args[3]
+
+            if "x" in kwargs:
+                if not isinstance(kwargs["x"], int):
+                    raise TypeError("x must be an integer")
+                if kwargs["x"] <= 0:
+                    raise ValueError("x must be >= 0")
+                self.__x = kwargs["x"]
+
+            if len(args) > 4:
+                if not isinstance(args[4], int):
+                    raise TypeError("y must be an integer")
+                if args[4] < 0:
+                    raise ValueError("y must be >= 0")
+                self.__y = args[4]
+
+            if "y" in kwargs:
+                if not isinstance(kwargs["y"], int):
+                    raise TypeError("y must be an integer")
+                if kwargs["y"] <= 0:
+                    raise ValueError("y must be >= 0")
+                self.__y = kwargs["y"]
