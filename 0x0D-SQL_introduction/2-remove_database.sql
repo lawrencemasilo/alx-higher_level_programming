7c213d0221caf63018654a1fda985a3a829d0b4a
Neo Masilo <neolawrencemasilo@gmail.com>
2023-10-18 07:55:16 +0000

    script that deletes a database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- script that deletes a database if it doesn't exist.
-DROP DATABASE IF NOT EXISTS hbtn_0c_0;
+-- script that deletes a database if it doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
