6395e71cbb3b4731ea40dd7c5ebcfa7bd2a15963
neo masilo <neolawrencemasilo@gmail.com>
2023-11-13 06:20:24 -0800

    a script that searches the second biggest integer in a list

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2 || process.argv[2] === 1) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2).map(Number);
+  array.sort((a, b) => b - a);
+  console.log(array[1]);
+}
