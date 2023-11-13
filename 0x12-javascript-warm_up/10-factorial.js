e82657432f32b33a14896d06d35f00b48ff97dc6
neo masilo <neolawrencemasilo@gmail.com>
2023-11-13 05:35:48 -0800

    a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (isNaN(a) || a === 0) {
+    return 1;
+  } else if (a < 0) {
+    return -1;
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
+console.log(factorial(Number(process.argv[2])));
