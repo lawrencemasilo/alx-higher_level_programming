b3e40478951a0f62b196a4e2d10e536a75888a4d
lawrencemasilo <66621783+lawrencemasilo@users.noreply.github.com>
2023-12-01 11:12:02 +0200

    Delete 0x10-python-network_0/1-body.sh

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o temp_response.txt -w "%{http_code}" "$1" > /dev/null && [[ $(cat temp_response.txt) -eq 200 ]] && curl -s "$1"
