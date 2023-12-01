0f5521574587eb13f0617948d6ba45541367878e
neo masilo <neolawrencemasilo@gmail.com>
2023-12-01 04:12:08 -0800

    python script that sends a POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module posts an email"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {'email': f'{sys.argv[2]}'}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('utf-8')
+
+    url = urllib.request.Request(url, data, method='POST')
+    with urllib.request.urlopen(url) as response:
+        email_data = response.read().decode('utf-8')
+
+    print(email_data)
