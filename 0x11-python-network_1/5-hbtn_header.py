226218f870a23023180fe30dab4ce2a77557ced7
neo masilo <neolawrencemasilo@gmail.com>
2023-12-01 05:52:36 -0800

    displays the value of the variable X-Request-Id in the response header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    url_r = requests.get(url)
-
-    print(url_r.headers['X-Request-Id'])
+    url_r = requests.get(url, auth=('user', 'pass'))
+    x_request_id = url_r.headers['X-Request-Id']
+    print(x_request_id)
