416c2507c8d0ca23ebdfd9c51167796f51be40f4
neo masilo <neolawrencemasilo@gmail.com>
2023-12-01 02:22:19 -0800

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """This module fetches a URL"""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    content = response.read()
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
 
-print('Body response:')
-print(f'    - type: {type(content)}')
-print(f'    - content: {content}')
-print(f'    - utf8 content: {content.decode("utf-8")}')
+    print('Body response:')
+    print(f'    - type: {type(content)}')
+    print(f'    - content: {content}')
+    print(f'    - utf8 content: {content.decode("utf-8")}')
