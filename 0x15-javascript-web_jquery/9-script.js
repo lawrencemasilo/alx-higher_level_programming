40f0bd15c92dca1810b92274afc5d361241ecbfa
neo masilo <neolawrencemasilo@gmail.com>
2024-01-10 06:20:40 -0800

    script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    success: function (data) {
+      $('#hello').text(data.hello);
+    }
+  });
+});
